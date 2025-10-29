7abb9c84ab9958b02d3954ec624e008ef1e6f53b
rexchoppers <rexchoppers@gmail.com>
2025-10-29 20:53:23 +0000

    WIP Model loading

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import os
 from multiprocessing import Process
 from typing import Optional
 
+from gpt4all import GPT4All
+from llama_cpp import Llama
+
+
+def run():
+    MODEL_PATH = "llms/phi-4-Q4_0.gguf"
+    # os.makedirs("models", exist_ok=True)
+
+    llm = Llama(
+        model_path=MODEL_PATH,
+        n_ctx=16000,
+        n_threads=4,  # adjust for your CPU cores
+    )
+
 if __name__ == "__main__":
+    run()
